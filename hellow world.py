8bfcd3915c15e678391dd8d3f65bb8610e45a7ab
kharisma <etieneobongeric@gmail.com>
2020-06-07 00:25:48 +0100

    hello world script

---
+++ hellow world.py
@@ -0,0 +1,12 @@
+first_name='Etiene'
+last_name='Eric'
+email='etieneobongeric@gmail.com'
+HNG_ID='HNG-06550'
+language='python'
+message=f'hello world, this is {first_name} {last_name} with HNGi7 ID {HNG_ID} and email {email} using {language} for stage 2 task'
+print(message)
+
+
+
+
+
